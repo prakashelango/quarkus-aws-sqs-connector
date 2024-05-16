29a4446aa7957c91bb69feb846d06f2ebbba0814
prakashelango <prakashcse100@gmail.com>
2024-05-16 17:11:46 +0800

    via event bus channel

--- src/main/java/org/acme/MyReactiveMessagingApplication.java
+++ src/main/java/org/acme/MyReactiveMessagingApplication.java
@@ -5,14 +5,20 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import io.quarkus.runtime.Startup;
+import io.quarkus.vertx.ConsumeEvent;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.aws.sqs.SqsOutboundMetadata;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.core.eventbus.EventBus;
 import lombok.RequiredArgsConstructor;
 
 import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Multi;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
 import org.eclipse.microprofile.reactive.messaging.Metadata;
@@ -27,11 +33,24 @@ public class MyReactiveMessagingApplication {
 
     private final EventBus usageQueueEventBus;
 
-    /*@Channel("out")
-    Emitter<String> sqsEmitter;*/
+    @Channel("out")
+    Emitter<String> sqsEmitter;
 
 
-   @Outgoing("out")
+    @ConsumeEvent("event_bus_channel")
+    public void generateStreamOfStrings(Domain domain) {
+        Log.infof("Consuming event bus message: %s", domain);
+
+        var json = JsonObject.mapFrom(domain);
+
+        String jsonString = json.toString();
+        Log.infof("Json Emitter: %s", jsonString);
+        sqsEmitter.send(jsonString);
+    }
+
+
+
+   /*@Outgoing("out")
     public Multi<Message<Domain>> produce() {
         Log.infof("Producer sending messages");
         return Multi.createFrom().ticks().every(Duration.of(300, ChronoUnit.MILLIS))
@@ -46,27 +65,16 @@ public class MyReactiveMessagingApplication {
                         return Uni.createFrom().voidItem().subscribeAsCompletionStage();
                     });
             });
-    }
+    }*/
 
-
-    /*@ConsumeEvent("event_bus_channel")
-    public void generateStreamOfStrings(List<UsgData> list) {
-        Log.infof("Consuming event bus message: %s", list);
-
-        var json = new JsonArray(list);
-
-        String jsonString = json.toString();
-        Log.infof("Json Emitter: %s", jsonString);
-        sqsEmitter.send(jsonString);
-    }
 
 
 
    @Startup
     public void playWithEvents() {
         Multi.createFrom().ticks().every(Duration.of(1, ChronoUnit.SECONDS))
-            .subscribe().with(item -> usageQueueEventBus.send("event_bus_channel", List.of(new UsgData("name"+item))));
-    }*/
+            .subscribe().with(item -> usageQueueEventBus.send("event_bus_channel", new Domain("name"+item)));
+    }
 
     @Incoming("in")
     public void consume(Domain in) {
